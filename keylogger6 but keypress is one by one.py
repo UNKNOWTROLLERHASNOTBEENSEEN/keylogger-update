9aaf1b4e6eef1275fb42aa7b83f3ae54d071e672
UNKNOWTROLLERHASNOTBEENSEEN <115999637+UNKNOWTROLLERHASNOTBEENSEEN@users.noreply.github.com>
2024-03-21 19:25:48 +0400

    Update keylogger6 but keypress is one by one.py

--- keylogger6 but keypress is one by one.py
+++ keylogger6 but keypress is one by one.py
@@ -2,7 +2,7 @@ import keyboard
 import datetime
 
 def on_key_event(event):
-    if event.event_type == 'up':  # Only print for key release events
+    if event.event_type == 'up': 
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"{current_time} - Key {event.name} released")
 
